946863acc885bc89b680fc58fb4001f331640775
amandatorresapu <69279617+amandatorresapu@users.noreply.github.com>
2021-07-06 21:37:26 -0700

    current temperature, wind, humidity

--- src/index.js
+++ src/index.js
@@ -1,1 +1,23 @@
+function displayTemperature(response) {
+  console.log(response.data);
+  console.log(response.data.main.temp);
+  let temperatureElement = document.querySelector("#temperature");
+  temperatureElement.innerHTML = Math.round(response.data.main.temp);
 
+  let cityElement = document.querySelector("#city");
+  cityElement.innerHTML = response.data.name;
+
+  let descriptionElement = document.querySelector("#description");
+  descriptionElement.innerHTML = response.data.weather[0].description;
+
+  let humidityElement = document.querySelector("#humidity");
+  humidityElement.innerHTML = response.data.main.humidity;
+
+  let windElement = document.querySelector("#wind");
+  windElement.innerHTML = Math.round(response.data.wind.speed);
+}
+
+let apiKey = "54a19520a091f37a82f561cb9de8d7b4";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=New York&appid=${apiKey}&units=imperial`;
+
+axios.get(apiUrl).then(displayTemperature);
