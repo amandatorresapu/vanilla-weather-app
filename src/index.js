802451ed03aa70495ad68336308c0387519873f9
amandatorresapu <69279617+amandatorresapu@users.noreply.github.com>
2021-07-07 21:51:32 -0700

    added search engine

--- src/index.js
+++ src/index.js
@@ -49,8 +49,20 @@ function displayTemperature(response) {
   iconElement.setAttribute("alt", response.data.weather[0].description);
 }
 
-let apiKey = "54a19520a091f37a82f561cb9de8d7b4";
-let city = "Merced";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=imperial`;
+function search(city) {
+  let apiKey = "54a19520a091f37a82f561cb9de8d7b4";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=imperial`;
+  axios.get(apiUrl).then(displayTemperature);
+}
 
-axios.get(apiUrl).then(displayTemperature);
+function handleSubmit(event) {
+  event.preventDefault();
+  let cityInputElement = document.querySelector("#city-input");
+  search(cityInputElement.value);
+  console.log(cityInputElement);
+}
+
+search("merced");
+
+let form = document.querySelector("#search-form");
+form.addEventListener("submit", handleSubmit);
