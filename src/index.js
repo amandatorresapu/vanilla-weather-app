23976efcc39289d67c0f46b704909d23fc8d2f5e
amandatorresapu <69279617+amandatorresapu@users.noreply.github.com>
2021-07-07 19:42:56 -0700

    removed console logs

--- src/index.js
+++ src/index.js
@@ -22,8 +22,6 @@ function formatDate(timestamp) {
 }
 
 function displayTemperature(response) {
-  console.log(response.data);
-  console.log(response.data.main.temp);
   let temperatureElement = document.querySelector("#temperature");
   temperatureElement.innerHTML = Math.round(response.data.main.temp);
 
