09504f560eaeb6cdae657f5eb9f971393cc1ca04
amandatorresapu <69279617+amandatorresapu@users.noreply.github.com>
2021-07-21 19:58:32 -0700

    ommited C items from js and html

--- src/index.js
+++ src/index.js
@@ -67,7 +67,7 @@ function displayForecast(response) {
 
 function getForecast(coordinates) {
   let apiKey = "54a19520a091f37a82f561cb9de8d7b4";
-  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=imperial`;
   axios.get(apiUrl).then(displayForecast);
 }
 
@@ -107,7 +107,7 @@ function displayTemperature(response) {
 
 function search(city) {
   let apiKey = "54a19520a091f37a82f561cb9de8d7b4";
-  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=imperial`;
   axios.get(apiUrl).then(displayTemperature);
 }
 
